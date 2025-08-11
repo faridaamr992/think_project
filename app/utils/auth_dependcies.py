f1dbd26ef4d4017f42ad19dedf13e4d4482da6b9
faridaamr992 <faridaamr992@yahoo.com>
2025-08-11 11:42:55 +0300

    change jwt from taking pass and username to token

--- app/utils/auth_dependcies.py
+++ app/utils/auth_dependcies.py
@@ -1,13 +1,14 @@
 from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import HTTPBearer
 from app.utils.jwt_handler import verify_token
 from app.container import container
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+security = HTTPBearer()
 
-async def get_current_user(token: str = Depends(oauth2_scheme)):
+async def get_current_user(credentials=Depends(security)):
+    token = credentials.credentials  # Extract token string
     payload = verify_token(token)
-    username: str = payload.get("sub")
+    username = payload.get("sub")
     if username is None:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
